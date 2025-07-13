3e25511755de7b34eed9ce8a7358af8217610c5f
Raneen593 <raneenelalfy0@gmail.com>
2025-07-14 01:36:32 +0300

    Add files via upload

---
+++ t2d1.php
@@ -0,0 +1,49 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Profile</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+</head>
+
+<body class="d-flex justify-content-center align-items-center vh-100 p-3 bg-success">
+    <div class="card shadow-lg p-4" style="width: 400px;">
+        <h4 class="text-center mb-3">User Profile</h4>
+
+        <div class="alert alert-success fw-bold mb-4">
+            Welcome, almost reckamed abubair!
+        </div>
+
+        <div class="border border-secondary rounded p-3 mb-4">
+            <h5 class="mb-3">User Information</h5>
+
+            <div class="mb-3 d-flex align-items-center">
+                <strong class="me-2">Full Name:</strong>
+                <span>Raneen Sherif Elalfy</span>
+            </div>
+            <hr>
+
+            <div class="mb-3">
+                <strong class="mb-2">Email:</strong>
+                <span>raneenelalfy0@gmail.com</span>
+            </div>
+            <hr>
+            <div class="mb-3 ">
+                <strong class=" mb-2">Age:</strong>
+                <span>20</span>
+            </div>
+            <hr>
+            <div class="mb-3">
+                <strong class="mb-2">City:</strong>
+                <span>Damitta</span>
+            </div>
+        </div>
+
+        <div class="text-center">
+            <a href="form.php" class="btn btn-primary ">Back to Form</a>
+        </div>
+    </div>
+
+    <script src="js/bootstrap.bundle.js"></script>
+</body>
+
+</html>
